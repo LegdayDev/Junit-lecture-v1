3937f89c84555af08a9ad9d5716a2e0e173ea118
JaeYoung <chlwodud0327@naver.com>
2023-01-19 22:17:15 +0900

    책등록 test 완료

--- src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
@@ -1,5 +1,7 @@
 package site.metacoding.junitproject.domain;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -7,16 +9,24 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 @DataJpaTest // DB와 관련된 컴포넌트만 메모리에 로딩
 public class BookRepositoryTest {
 
-    // @Autowired
-    // private BookRepository bookRepository;
+    @Autowired
+    private BookRepository bookRepository;
 
     @Test
     public void saveTest() {
         // given(데이터 준비)
-
+        String title = "junit5";
+        String author = "메타코딩";
+        Book book = Book.builder()
+                .title(title)
+                .author(author)
+                .build();
         // when(테스트 실행)
+        Book bookPS = bookRepository.save(book);
 
-        // then(검증))
+        // then(검증)
+        assertEquals(title, bookPS.getTitle());
+        assertEquals(author, bookPS.getAuthor());
 
     }
     // 1. 책 등록
